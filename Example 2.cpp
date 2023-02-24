9c7237b2597da0db8aed1c808e6f922026bea276
Artem <92841151+ArtemBystrovOfficial@users.noreply.github.com>
2023-02-25 01:15:43 +0800

    Add files via upload

---
+++ Example 2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <limits>
+#include <type_traits>
+#include <string>
+
+struct Impl {
+    int value;
+    template<class R>
+    operator R() const {
+        if constexpr (std::is_floating_point_v<R>)
+            return (float)value;
+        else if constexpr (std::is_same_v<R, std::string>)
+            return std::to_string(value);
+        else if constexpr (std::is_integral_v<R>)
+            return value;
+    };
+};
+
+auto func(int value) {
+    return Impl{ value };
+}
+
+int main() {
+    std::string s = func(1); // "1"
+    float f = func(2);       // 2.0
+    int i = func(3);         // 3
+    bool g = func(3);
+    std::cout << s;
+}
