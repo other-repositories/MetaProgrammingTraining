8455575ed1f6f8604841991923740c92c54a48b8
Artem <92841151+ArtemBystrovOfficial@users.noreply.github.com>
2023-02-25 01:27:30 +0800

    Add files via upload

---
+++ Example 3.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <limits>
+#include <type_traits>
+#include <string>
+
+
+struct ToolTypeA {
+    int t;
+};
+
+struct ToolTypeB {
+    std::string b;
+    std::string g;
+};
+
+struct ToolTypeC {
+    float b;
+    double g;
+};
+
+struct FabricStatic {
+    template<class T>
+    operator T() const {
+        if constexpr (std::is_same_v<T,ToolTypeA>)
+            return ToolTypeA{5};
+        else if constexpr (std::is_same_v<T,ToolTypeB>)
+            return ToolTypeB{"24","124"};
+        else if constexpr (std::is_same_v<T,ToolTypeC>)
+            return ToolTypeC{5.7,3.4};
+    };
+};
+
+auto getToolStatic() {
+    return FabricStatic{};
+}
+
+int main() {
+    ToolTypeB b = getToolStatic(); // "1"
+}
