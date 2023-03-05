acc1b69b19aa3f061659048abdc0d5d0d88cbf06
Artem <92841151+ArtemBystrovOfficial@users.noreply.github.com>
2023-03-05 21:34:06 +0800

    Add files via upload

---
+++ StrategyByStaticPol.cpp
@@ -0,0 +1,41 @@
+﻿#include <string>
+#include <iostream>
+
+//CRTP
+template <class Derived>
+struct BasicStrategy {
+	void foo() {
+		static_cast<const Derived*>(this)->foo();
+	}
+};
+
+struct StrategyA : public BasicStrategy<StrategyA> {
+	void foo() {
+		std::cout << "test1";
+	}
+};
+
+struct StrategyB : public BasicStrategy<StrategyB> {
+	void foo() {
+		std::cout << "test2";
+	}
+};
+
+template <class Strat, typename = std::enable_if_t<std::is_base_of_v<BasicStrategy<Strat>,Strat> > >
+class A {
+public:
+	void foo() {
+		Strat _strat;
+		_strat.foo();
+	}
+private:
+
+};
+
+
+void main() {
+	A<StrategyA> out1;
+	A<StrategyB> out2;
+	out1.foo();
+	out2.foo();
+}
